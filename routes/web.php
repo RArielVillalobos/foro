07a0c07495c9f33939becca403f240f6a3f73477
RArielVillalobos <ariel.villalobos96@gmail.com>
2018-05-05 15:38:10 -0300

    Agregando tabla foro

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/','ForumController@index')->name('forum.index');
